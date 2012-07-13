7a5132d5dbed5913c104aa1af8b6436265c57f87
Asumu Takikawa <asumu@ccs.neu.edu>
2012-07-13 15:14:06 -0400

    Update for PLaneT

--- info.rkt
+++ info.rkt
@@ -1,4 +1,46 @@
 #lang setup/infotab
 
-(define mzscheme-launcher-libraries (list "start.rkt"))
-(define mzscheme-launcher-names (list "tex2page"))
+(define racket-launcher-libraries (list "start.rkt"))
+(define racket-launcher-names (list "tex2page"))
+
+(define release-notes '("Initial release of the package"))
+
+(define mcfly-planet       'asumu/tex2page:1:0)
+
+(define name               "tex2page")
+
+(define mcfly-subtitle     "Dorai Sitaram's tool to convert TeX to web pages")
+
+(define blurb              (list name ": " mcfly-subtitle))
+
+(define homepage           "http://www.ccs.neu.edu/home/dorai/tex2page/")
+
+(define mcfly-author       "Asumu Takikawa & Dorai Sitaram")
+
+(define repositories       '("4.x"))
+
+(define categories         '(misc))
+
+(define can-be-loaded-with 'all)
+
+(define scribblings        '(("doc.scrbl" () (library))))
+
+(define primary-file       "main.rkt")
+
+(define mcfly-start        "main.rkt")
+
+(define mcfly-files        '(defaults
+                              "start.rkt"
+                              "tex2page-aux.rkt"
+                              "tex2page.rkt"
+                              "tex2page.sty"
+                              "tex2page.tex"))
+
+(define mcfly-license      "")
+
+(define mcfly-legal        (string-append "Copyright (c) 1997-2009, Dorai Sitaram."
+                                          "All rights reserved."
+                                          "Permission to distribute and use this work for any"
+                                          "purpose is hereby granted provided this copyright"
+                                          "notice is included in the copy.  This work is provided"
+                                          "as is, with no warranty of any kind."))
--- main.rkt
+++ main.rkt
@@ -1,4 +1,20 @@
-#lang scheme/base
+#lang racket/base
 
-(require "tex2page.rkt")
-(provide (all-from-out "tex2page.rkt"))
+(require (planet neil/mcfly))
+
+;; this file is not meant to be run
+;; one possibility is that it could install the binaries when run
+
+(doc (para "This package provides Dorai Sitaram's tex2page tool. "
+           "The tool used to be bundled with Racket and is now provided "
+           "here for legacy purposes. For more "
+           "details and the latest version of this software, see the tool's website linked "
+           "above.")
+     (para "When you install this PLaneT package with the "
+           (racket require)
+           " command above, the binary for tex2page should be installed "
+           "in the bin/ directory of your Racket install."))
+
+(doc history
+     (#:planet 1:0 #:date "2012-07-13"
+               "Initial move."))
